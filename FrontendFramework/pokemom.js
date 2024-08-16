e315ac395600148178eb8a4283f8b71a6a5a0754
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-16 20:25:33 -0300

    Switch Case

---
+++ FrontendFramework/pokemom.js
@@ -0,0 +1,17 @@
+const pokemon = "Bulbasauro"
+
+switch (pokemon) {
+    case "Bulbasauro":
+        console.log("Tipo: planta e veneno")
+        break;
+    case "Charmander":
+        console.log("Tipo: fogo")
+        break;
+    case "Squirtle":
+        console.log("Tipo: água")
+        break;
+
+    default:
+        console.log("Pokemon não encontrado")
+        break;
+}
