159d0144af82b77164ac065a2079a8fe033be934
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-16 20:02:00 -0300

    IF/ELSE/IF

--- FrontendFramework/status.js
+++ FrontendFramework/status.js
@@ -6,9 +6,19 @@ const num2 = Number(prompt("Digite outro número: "))
 
 //console.log(num1, num2)
 
-if (num1 === num2) {
-    console.log("Os números são iguais!")
+//Retorna mensagens dizendo se os números são iguais ou diferentes
+// if (num1 === num2) {
+//     console.log("Os números são iguais!")
     
-} else {
-    console.log("Os números são diferentes!")
+// } else {
+//     console.log("Os números são diferentes!")
+// }
+
+//Se o primeiro número é {maior/menor/igual} ao segundo número
+if (num1>num2) {
+    console.log("num1 é maior que num2")
+} else if(num1<num2){
+    console.log("num1 é menor que num2")
+} else{
+    console.log("num1 é igual a num2")
 }
