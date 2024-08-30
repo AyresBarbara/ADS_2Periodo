58008f8c3a7192048384669e9400d576a41c755d
BARBARA FERNANDA AYRES DA SILVA <01146760@sempreuninassau.com.br>
2024-08-30 19:31:46 -0300

    Funções

---
+++ FrontendFramework/laços/exercicio01.js
@@ -0,0 +1,10 @@
+//Receba 2 numeros e faça a soma deles
+function somaNum(num1, num2) {
+    const soma = num1 + num2
+    console.log("A soma é: "+ soma)
+    return soma
+}
+//somaNum(5,7)
+
+const soma = somaNum(5,7)
+console.log(soma)
