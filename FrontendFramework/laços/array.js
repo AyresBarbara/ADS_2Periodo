1727ff287a6031897ccc263e37af51402fe8f244
AyresBarbara <ayresbarbara2@gmail.com>
2024-08-30 19:46:38 -0300

    Função Array

---
+++ FrontendFramework/laços/array.js
@@ -0,0 +1,11 @@
+function lerArrays (array) {
+    for (const element of array) {
+        console.log(element)
+    }
+    const tamanhoArray = array.length
+
+    return tamanhoArray
+}
+
+const meuArray = [1, 2, 3, 4, 5, 6]
+console.log(lerArrays(meuArray))
