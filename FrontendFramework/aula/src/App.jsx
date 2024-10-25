caed2e1f53b39317a92730a46813af3a24a681ed
AyresBarbara <ayresbarbara2@gmailcom>
2024-10-25 20:55:01 -0300

    React

--- FrontendFramework/aula/src/App.jsx
+++ FrontendFramework/aula/src/App.jsx
@@ -8,8 +8,19 @@ function App() {
     <div className="App">
       {/*Formas de chanmar meu componente*/ }
       {/*<MeuPrimeiroComponente/>*/}
-      <MeuPrimeiroComponente></MeuPrimeiroComponente>
-      <Noticia></Noticia>
+      {/*<MeuPrimeiroComponente></MeuPrimeiroComponente>*/}
+      <Noticia 
+      title = {"Primeira Noticia"}
+      subTitle = {"Primeiro subtitulo"}
+      image = {"https://picsum.photos/id/237/400"}
+      content ={"Conteudo da primeira Noticia"}
+      />
+      <Noticia 
+      title = {"Segunda Noticia"}
+      subTitle = {"Segundo subtitulo"}
+      image = {"https://fastly.picsum.photos/id/1084/536/354.jpg?grayscale&hmac=Ux7nzg19e1q35mlUVZjhCLxqkR30cC-CarVg-nlIf60"}
+      content ={"Conteudo da primeira Noticia"}
+      />
     </div>
   );
 }
--- FrontendFramework/aula/src/components/Noticia.jsx
+++ FrontendFramework/aula/src/components/Noticia.jsx
@@ -3,10 +3,13 @@ import React from "react";
 const Noticia = (props) => {
     return(
         <div>
-            <h1>Título da noticia</h1>
-            <h3>Subtítulo da Noticia</h3>
-            <img src="https://www.voltologo.net/wp-content/uploads/2023/01/recife-antigo-onde-fica.jpg" alt="" />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia, perferendis possimus eum in hic neque quaerat placeat laboriosam ipsum dicta ea dolorum officiis reiciendis aliquid maxime nemo vel, soluta adipisci?</p>
+            {/*<h1>Título da noticia</h1>*/}
+            {/*<h3>Subtítulo da Noticia</h3> */}
+            {/*<img src="https://www.voltologo.net/wp-content/uploads/2023/01/recife-antigo-onde-fica.jpg" alt="" />*/}
+            <h1>{props.title}</h1> {/*Props é um objeto*/}
+            <h3>{props.subTitle}</h3>
+            <img src={props.image} alt="" />
+            <p>{props.content}</p>
 
         </div>
     )
